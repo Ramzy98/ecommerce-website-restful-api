de74581b436f68271e8e07b1870075b593db4f74
Ramzy98 <ahmadramzy988@gmail.com>
2022-05-13 21:24:30 +0200

    update testing logic

--- src/tests/orders_spec.ts
+++ src/tests/orders_spec.ts
@@ -1,6 +1,6 @@
 import { Order, OrderStore } from '../models/order';
 import { User, UserStore } from '../models/user';
-
+import { Product, ProductStore } from '../models/product';
 const store = new OrderStore();
 
 const userStore = new UserStore();
@@ -80,6 +80,41 @@ describe('OrderStore', () => {
       expect(order.status).toBeDefined();
 
       expect(order.quantity).toBeDefined();
+    });
+  });
+
+  describe('getOrdersByUserId', () => {
+    it('should return all orders by user id', async () => {
+      const orders = await store.getOrdersByUserId(user.id);
+      expect(orders).toBeDefined();
+    });
+  });
+
+  describe('getOrdersByStatus', () => {
+    it('should return all orders by status', async () => {
+      const orders = await store.getOrdersByStatus('active');
+      expect(orders).toBeDefined();
+    });
+  });
+
+  describe('addProductToOrder', () => {
+    it('should add a product to an order', async () => {
+      const order = await store.create({
+        user_id: user.id,
+        status: 'active',
+        quantity: 1,
+      } as Order);
+      const createdProduct = await new ProductStore().create({
+        name: 'New Product',
+        price: 100,
+        category: 'Electronics',
+      } as Product);
+      const product = await store.addProductToOrder(
+        order.id,
+        createdProduct.id,
+        1
+      );
+      expect(product).toBeDefined();
     });
   });
 });
