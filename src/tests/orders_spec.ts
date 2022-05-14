4ef3df0d74730a615be21f3997773a2c141be037
Ramzy98 <ahmadramzy988@gmail.com>
2022-05-14 15:16:57 +0200

    added order endpoint testing

--- src/tests/orders_spec.ts
+++ src/tests/orders_spec.ts
@@ -1,7 +1,11 @@
 import { Order, OrderStore } from '../models/order';
 import { User, UserStore } from '../models/user';
 import { Product, ProductStore } from '../models/product';
+import request from 'supertest';
+import express from 'express';
+
 const store = new OrderStore();
+const app = express();
 
 const userStore = new UserStore();
 async function createUser() {
@@ -31,7 +35,19 @@ describe('OrderStore', () => {
       expect(order.status).toBeDefined();
       expect(order.quantity).toBeDefined();
     });
+
+    it('should not create an order using the endpoint', async () => {
+      request(app)
+        .post('/orders')
+        .send({
+          user_id: user.id,
+          status: 'active',
+          quantity: 1,
+        })
+        .expect(200);
+    });
   });
+
   describe('index', () => {
     it('should return all orders', async () => {
       const orders = await store.index();
@@ -45,7 +61,12 @@ describe('OrderStore', () => {
       expect(order.status).toBeDefined();
       expect(order.quantity).toBeDefined();
     });
+
+    it('should not return all orders using the endpoint', async () => {
+      request(app).get('/orders').expect(200);
+    });
   });
+
   describe('show', () => {
     it('should return an order', async () => {
       const order = await store.show(1);
@@ -56,7 +77,12 @@ describe('OrderStore', () => {
       expect(order.status).toBeDefined();
       expect(order.quantity).toBeDefined();
     });
+
+    it('should not return an order using the endpoint', async () => {
+      request(app).get('/orders/1').expect(200);
+    });
   });
+
   describe('update', () => {
     it('should update an order', async () => {
       const order = await store.update(1, {
@@ -70,7 +96,19 @@ describe('OrderStore', () => {
       expect(order.status).toBeDefined();
       expect(order.quantity).toBeDefined();
     });
+
+    it('should not update an order using the endpoint', async () => {
+      request(app)
+        .put('/orders/1')
+        .send({
+          user_id: user.id,
+          status: 'active',
+          quantity: 1,
+        })
+        .expect(200);
+    });
   });
+
   describe('delete', () => {
     it('should delete an order', async () => {
       const order = await store.delete(1);
@@ -81,6 +119,10 @@ describe('OrderStore', () => {
 
       expect(order.quantity).toBeDefined();
     });
+
+    it('should not delete an order using the endpoint', async () => {
+      request(app).delete('/orders/1').expect(200);
+    });
   });
 
   describe('getOrdersByUserId', () => {
@@ -88,12 +130,20 @@ describe('OrderStore', () => {
       const orders = await store.getOrdersByUserId(user.id);
       expect(orders).toBeDefined();
     });
+
+    it('should not return all orders by user id using the endpoint', async () => {
+      request(app).get('/orders/user/1').expect(200);
+    });
   });
 
   describe('getOrdersByStatus', () => {
     it('should return all orders by status', async () => {
       const orders = await store.getOrdersByStatus('active');
       expect(orders).toBeDefined();
+    });
+
+    it('should not return all orders by status using the endpoint', async () => {
+      request(app).get('/orders/status/active').expect(200);
     });
   });
 
@@ -115,6 +165,15 @@ describe('OrderStore', () => {
         1
       );
       expect(product).toBeDefined();
+    });
+
+    it('should not add a product to an order using the endpoint', async () => {
+      request(app)
+        .post('/orders/1/products/1')
+        .send({
+          quantity: 1,
+        })
+        .expect(200);
     });
   });
 });
